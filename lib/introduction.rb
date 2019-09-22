c8abd14b8193ce5cedf8fd4b7fbc58cf72ecf0ba
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-22 23:50:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,4 @@
-# def introduction(name)
-#   puts "Hi, my name is Lakisha."
-#end
 
-# def introduction_with_language(name,language)
-#   puts "Hi, my name is #{Lakisha} and I am learning to program in #{Ruby}."
-#end
-
-#introduction_with_language_optional(name, language="Ruby")
-#   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
 
 
 
